b099911768bd74eed104462cc169492b678c4377
Hobbyist <hilch1981@gmail.com>
2026-08-25 10:55:10 +0900

    i18n: register integrations locale data

--- AzerothAdmin/Modules/Integrations/Registration.lua
+++ AzerothAdmin/Modules/Integrations/Registration.lua
@@ -3,10 +3,10 @@ local addon = AzerothAdminEasy
 if not addon.RegisterModule then return end
 
 addon:RegisterModule("integrations", {
-    status = "module-active",
-    dependencies = { "shell", "item-browser", "profession-info" },
+    status = "module-active-localized",
+    dependencies = { "shell", "item-browser", "profession-info", "language" },
     runtimeFiles = { "AzerothAdmin/Modules/Integrations/Module.lua", "AzerothAdmin/Modules/Integrations/Registration.lua" },
-    dataFiles = {},
+    dataFiles = { "AzerothAdmin/Modules/Integrations/Locales" },
     tests = { "tools/test_craft_frame_lifecycle.py", "tools/test_remote_bank_flow.py" },
     futurePath = "AzerothAdmin/Modules/Integrations",
 })
